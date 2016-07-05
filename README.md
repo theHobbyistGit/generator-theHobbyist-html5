4d4878e284e98cbde718546cbd850d3b48fc5379
theHobbyistGit <the.hobbyist@gmx.de>
2016-07-06 00:30:21 +0200

    added some not working code ... will check later why it is not working

---
+++ README.md
@@ -0,0 +1,9 @@
+# Yeoman Generator for a simple HTML5 boilerplate
+
+Here are some basic features i would like to have in this Generator:
+
+* using twitter bootstrap or not
+* using sass or not
+* having a gruntfile or not
+* using jquery or not
+* Entering a name for the new page
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,18 +1,34 @@
+'use strict';
 var generators = require('yeoman-generator');
 var yosay = require('yosay');
 
 module.exports = generators.Base.extend({
-  /*constructor : function(){
-    generators.Base.apply(this, args);
 
-    //Custom Code here
-
-
-  }*/
-  method1: function(){
-    this.log(yosay("This is an \'allo \'allo test..."));
+  initializing: function(){
+    this.log(yosay("\'Allo \'allo I will create your HTML5 Boilerplate..."));
   },
-  method2: function(){
-    console.log("test2");
-  }
+
+  prompting: function() {
+    var done = this.async();
+    this.prompt({
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      //Defaults to the project's folder name if the input is skipped
+      default: this.appname
+    }, function(answers) {
+      this.props = answers
+      this.log(answers.name);
+      done();
+    }.bind(this));
+  },
+
+  writing: function(){
+    this.fs.copyTpl(
+      this.templatePath('_page/_index.html'),
+      this.destinationPath('index.html'),
+      { title: "answers.name" }
+    );
+  },
+
 });
