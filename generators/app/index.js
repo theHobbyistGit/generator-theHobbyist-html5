f204fad83a441b0483205f5af0d131cbdce977b8
Stoffel <tstoffel@eventim.ag>
2016-08-03 09:02:45 +0200

    changed to promise instead of old implementation

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,17 +9,15 @@ module.exports = generators.Base.extend({
   },
 
   prompting: function() {
-    var done = this.async();
-    this.prompt({
+    return this.prompt({
       type: 'input',
       name: 'name',
       message: 'Your project name',
       //Defaults to the project's folder name if the input is skipped
       default: this.appname
-    }, function(answers) {
+    }).then(function(answers) {
       this.props = answers
       this.log(answers.name);
-      done();
     }.bind(this));
   },
 
